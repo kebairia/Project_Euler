abb773234b7a9d5fd06f0121f8c8c2af7c81ea4e
Zakaria Kebairia <4.kebairia@gmail.com>
2021-10-25 03:52:55 +0100

    source code for problem 3

--- python/p003.py
+++ python/p003.py
@@ -1,3 +1,26 @@
 #!/bin/env python
+from math import *
+
+def is_prime(x):
+    if x == 1 :
+        return False
+    if x == 2:
+        return True
+    else:
+        for i in range(2, x):
+            if x%i == 0 :
+                return False
+            return True
 def prime_fact(x):
-    return x
+    lst = []
+    for i in range(1, x):
+        if is_prime(i) and x%i == 0 :
+                lst.append(i)
+                x = x/i
+    lst.append(x)
+
+    print(max(lst))
+    print(lst)
+# prime_fact(600851475143)
+prime_fact(60085147)
+#
